972729b72fc73d2a6ff8c950e01f8672ecd32d57
cloudpunchx <siobhanbonardi@hotmail.com>
2024-01-08 22:43:53 -0700

    Pip freeze requirements.txt for launch.

--- run.py
+++ run.py
@@ -2,11 +2,12 @@ from app import app
 from flask_cors import CORS
 from dbcreds import production_mode
 
-# using port 5021
+# using port 5023
 if production_mode == True:
     print("Running server in Production Mode")
     import bjoern  # type:ignore
-    bjoern.run(app, "0.0.0.0", 5021)
+
+    bjoern.run(app, "0.0.0.0", 5023)
 else:
     print("Running Server in Testing Mode.")
     from flask_cors import CORS
